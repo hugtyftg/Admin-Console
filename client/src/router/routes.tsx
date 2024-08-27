7106bcd3e53275da814e63098b57902daa570953
hugtyftg <15224924822@163.com>
2024-08-27 13:53:11 +0800

    feat: lazyload

--- client/src/router/routes.tsx
+++ client/src/router/routes.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-import Dashboard from '@/pages/dashboard';
+import React, { lazy, Suspense } from 'react';
 import { Navigate } from 'react-router-dom';
-import Products from '@/pages/products';
-import Customers from '@/pages/customers';
-import Transactions from '@/pages/transactions';
-import Geography from '@/pages/geography';
-import Overview from '@/pages/overview';
 
 enum RoutePath {
   DASHBOARD = '/dashboard',
@@ -14,39 +8,53 @@ enum RoutePath {
   TRANSACTIONS = '/transactions',
   GEOGRAPHY = '/geography',
   OVERVIEW = '/overview',
+  DAILY = '/daily',
   ROOT = '/',
 }
 
+const lazyLoad = (path: string) => {
+  const Component = lazy(() => import(path));
+  return (
+    <Suspense fallback={<h3>Loading...</h3>}>
+      <Component />
+    </Suspense>
+  );
+};
+
 interface RouteConfig {
   path: RoutePath;
-  element: React.ReactNode;
+  element: React.JSX.Element;
   children?: RouteConfig[];
 }
 
 const routes: RouteConfig[] = [
   {
     path: RoutePath.DASHBOARD,
-    element: <Dashboard />,
+    element: lazyLoad('../pages/dashboard'),
   },
   {
     path: RoutePath.PRODUCTS,
-    element: <Products />,
+    element: lazyLoad('../pages/products'),
   },
   {
     path: RoutePath.CUSTOMERS,
-    element: <Customers />,
+    element: lazyLoad('../pages/customers'),
   },
   {
     path: RoutePath.TRANSACTIONS,
-    element: <Transactions />,
+    element: lazyLoad('../pages/transactions'),
   },
   {
     path: RoutePath.GEOGRAPHY,
-    element: <Geography />,
+    element: lazyLoad('../pages/geography'),
   },
   {
     path: RoutePath.OVERVIEW,
-    element: <Overview />,
+    element: lazyLoad('../pages/overview'),
+  },
+  {
+    path: RoutePath.DAILY,
+    element: lazyLoad('../pages/daily'),
   },
   {
     path: RoutePath.ROOT,
