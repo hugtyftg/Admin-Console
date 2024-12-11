cb3d1d14ae4abb27e415ad0375938a1bbd472803
hugtyftg <15224924822@163.com>
2024-12-11 17:26:51 +0800

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
+    // vite解析路径别名
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
