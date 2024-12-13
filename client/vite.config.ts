a799ca7182de65074e63954e1dc2d96dd1989a86
hugtyftg <15224924822@163.com>
2024-12-13 13:35:17 +0800

    feat: visual analysis during build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,14 @@ export default defineConfig({
       babel: {
         plugins: ['@emotion/babel-plugin'],
       },
+    }),
+    // 分析产物体积
+    visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: false,
+      filename: 'test.html', //分析图生成的文件名
+      open: true, //如果存在本地服务端口，将在打包后自动展示
     }),
   ],
 });
